edaaf9ef40558b99a59297a8d21a8379bb740e04
matfaisal <muh.faisal572@gmail.com>
2021-08-13 09:46:47 +0700

    API endpoint

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    public function index(){
+        $data = Post::all();
+
+        return response()->json($data, 200);
+    }
+
+    public function show($id) {
+        $data = Post::find($id);
+
+        return response()->json($data, 200);
+    }
+
+    public function store(Request $request) {
+
+        $data = $request->all();
+        Post::create($data);
+        return response()->json('success', 201);
+    }
+}
